fa727d4d7604932a11287bba6b75339e1ae25a6c
umerxgit2004 <umer.workplace174@gmail.com>
2024-02-25 14:31:55 +0500

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import e from 'express'
+dotenv.config()
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log("Connected to MongoDb")
+})
+.catch ((err)=>{
+    console.log(err)
+})
 const app = express()
 
 app.listen(3001,()=>{console.log("server running on Port 3001")})
