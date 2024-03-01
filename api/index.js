c1e9318a83039a79c7da003af8837e86f3c37ab2
umerxgit2004 <umer.workplace174@gmail.com>
2024-03-01 16:34:25 +0500

    create update user api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import e from 'express'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 mongoose.connect(process.env.MONGO)
 .then(() => {
@@ -14,11 +15,12 @@ mongoose.connect(process.env.MONGO)
 })
 const app = express()
 app.use(express.json())
-
+app.use(cookieParser())
 app.listen(3001,()=>{console.log("server running on Port 3001")})
 
 app.use("/api/user",userRoutes)
 app.use("/api/auth",authRoutes)
+
 app.use((err,req,res,next)=>{
     const statusCode = err.status || 500
     const message = err.message || "Something went wrong"
