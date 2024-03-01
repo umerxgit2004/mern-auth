3680077f561f3bfac44b3d87682989fad56cf6c1
umerxgit2004 <umer.workplace174@gmail.com>
2024-03-01 14:34:11 +0500

    upload img funct fix

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -49,7 +49,7 @@ export default function Profile() {
       <form className="flex flex-col gap-4">
         <input hidden type = "file" ref={fileRef} accept="image/*"
         onChange={(e) => setImage(e.target.files[0])}/>
-        <img onClick={() => fileRef.current.click()} src={currentUser.profilePicture} alt="profile" className="mt-2 h-24 w-24 self-center cursor-pointer rounded-full object-cover" />
+        <img onClick={() => fileRef.current.click()} src={formData.profilePicture || currentUser.profilePicture} alt="profile" className="mt-2 h-24 w-24 self-center cursor-pointer rounded-full object-cover" />
         <p className='text-sm self-center'>
           {imageError ? (
             <span className='text-red-700'>
